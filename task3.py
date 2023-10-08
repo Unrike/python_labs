fbf0d33d50b1657370502820466a46f5e684c04b
Unrike <90027129+Unrike@users.noreply.github.com>
2023-10-08 17:41:05 +0300

    Delete task3.py

--- task3.py
+++
@@ -1,5 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-num_of_players = round(len(list_players)/2)
-print(list_players[:num_of_players])
-print(list_players[num_of_players:])
