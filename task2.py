a85aa242cefe467206f35cbc30d1edc3fd8df6f6
Unrike <90027129+Unrike@users.noreply.github.com>
2023-10-08 17:40:57 +0300

    Delete task2.py

--- task2.py
+++
@@ -1,8 +0,0 @@
-memory = 1.44
-pages = 100
-lines = 50
-symbols = 25
-coding = 4
-result = round(memory * 1024 * 1024 / pages / lines / symbols / coding)
-
-print("Количество книг, помещающихся на дискету:", result)
