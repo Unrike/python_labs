addd00240b61949d4f3c0094dcbcec609f79220f
Unrike <90027129+Unrike@users.noreply.github.com>
2023-10-08 17:43:09 +0300

    lab1

---
+++ lab1/task3.py
@@ -0,0 +1,5 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+num_of_players = round(len(list_players)/2)
+print(list_players[:num_of_players])
+print(list_players[num_of_players:])
