8c3449e0c5987fbac889c824bfa64392a723bd70
Unrike <90027129+Unrike@users.noreply.github.com>
2023-10-18 20:14:52 +0300

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,9 +1,12 @@
+from math import floor
+
 memory = 1.44
 pages = 100
 lines = 50
 symbols = 25
 coding = 4
 mem_capacity = 1024
-result = round(memory * mem_capacity * mem_capacity / pages / lines / symbols / coding)
+result = floor(memory * mem_capacity * mem_capacity / pages / lines / symbols / coding)
 
 print("Количество книг, помещающихся на дискету:", result)
+
