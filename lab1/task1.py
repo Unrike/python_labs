86fc187f96a011606a9652020f9b7eaf7863dc6a
Unrike <90027129+Unrike@users.noreply.github.com>
2023-10-18 18:46:44 +0300

    Update task1.py

--- lab1/task1.py
+++ lab1/task1.py
@@ -1,5 +1,6 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-part_of_numbers = numbers[:4] + numbers[5:]
-numbers[4] = sum(part_of_numbers) / len(numbers)
+MISSED_ELEM_INDEX = 4
+part_of_numbers = numbers[:MISSED_ELEM_INDEX] + numbers[MISSED_ELEM_INDEX+1:]
+numbers[MISSED_ELEM_INDEX] = sum(part_of_numbers) / len(numbers)
 print("Измененный список:", numbers)
