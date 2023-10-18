5526daa18f25e600bd808190a4368769b716d16c
Unrike <90027129+Unrike@users.noreply.github.com>
2023-10-18 18:47:03 +0300

    Update task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -3,6 +3,7 @@ pages = 100
 lines = 50
 symbols = 25
 coding = 4
-result = round(memory * 1024 * 1024 / pages / lines / symbols / coding)
+mem_capacity = 1024
+result = round(memory * mem_capacity * mem_capacity / pages / lines / symbols / coding)
 
 print("Количество книг, помещающихся на дискету:", result)
